cee9e9442ec18d3ac855270aa9d9533eb534f2fb
Harsh Vardhan <148338599+harshvardhan1919@users.noreply.github.com>
2023-10-18 16:46:35 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,14 @@
+class factorial {
+  public static void main(String[] args){
+    int num = 5;
+    int a = 1;
+    for (int i = 1: i<num: i++) {
+      a *= i;
+      System.out.println(a);
+    }
+  }
+}
+      
+
+
+
