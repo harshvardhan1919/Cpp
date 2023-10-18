49811ff8e1d290a19f6c2bb3b0adca25ebb7ba56
Harsh Vardhan <148338599+harshvardhan1919@users.noreply.github.com>
2023-10-18 11:22:48 +0000

    made some changes

--- factorial.java
+++ factorial.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
 class factorial {
   public static void main(String[] args){
-    int num = 5;
+    Scanner m = new Scanner(System.in);
+    System.out.println("Enter number: ");
+    int num = m.nextInt();
+    // int num = 5;
     int a = 1;
-    for (int i = 1: i<num: i++) {
+    for (int i = 1; i<=num; i++) {
       a *= i;
-      System.out.println(a);
     }
+    System.out.println("factorial of " + num +" is "+a);
   }
 }
       
